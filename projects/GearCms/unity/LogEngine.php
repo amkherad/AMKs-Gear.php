7fc369d224294aac2ebb5c5ce6dcf3e1397b40ee
Ali Mousavi Kherad <alimousavikherad@gmail.com>
2016-07-23 02:57:58 -0700

    NICE for today !!! :love:

--- projects/GearCms/unity/LogEngine.php
+++ projects/GearCms/unity/LogEngine.php
@@ -3,6 +3,6 @@ class LogEngine implements IGearLogger
 {
     function write($mixed, $category = null)
     {
-        file_put_contents('sitelog.log', "$mixed\n", FILE_APPEND);
+        file_put_contents('sitelog.log', "$mixed\r\n", FILE_APPEND);
     }
 }
